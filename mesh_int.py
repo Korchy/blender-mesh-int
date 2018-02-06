1ed1da3a82d27d59528be5da3ab0742a17939a08
Nikita Akimov <korchiy@yandex.ru>
2018-02-06 22:43:49 +0300

    update

--- mesh_int.py
+++ mesh_int.py
@@ -37,21 +37,21 @@ class MeshIntPolygonRotateFollowActive(bpy.types.Operator):
             activeobjectmode = context.active_object.mode
             if activeobjectmode == 'EDIT':
                 bpy.ops.object.mode_set(mode='OBJECT')
-            polygonactive = bpy.context.active_object.polygons.active
-            # направление полигона (берем первый отрезок, точки 0 - 1)
-            polygonactivedirection =  Vector(context.active_object.vertices[context.active_object.polygons[polygonactive].vertices[0]].co - context.active_object.vertices[context.active_object.polygons[polygonactive].vertices[1]].co)
-            print(polygonactivedirection)
-
-            # bpy.data.objects['Cube'].dupli_list_create(bpy.data.scenes[0])
-            # [ob for ob in bpy.data.objects['Cube'].dupli_list]
-
-
+            polygonactive = bpy.context.active_object.data.polygons.active
+            # polygon direction - vector from polygon vertex 0 to polygon vertex 1
+            polygonactivedirection = context.active_object.data.vertices[context.active_object.data.polygons[polygonactive].vertices[1]].co - context.active_object.data.vertices[context.active_object.data.polygons[polygonactive].vertices[0]].co
+            polygonactivedirection.normalize()
             for polygon in bpy.context.active_object.data.polygons:
-                if polygon.select:
-                    if context.window_manager.mesh_int_vars.polygonrotatedirection == 'ccw':
-                        polygon.vertices = polygon.vertices[1:] + polygon.vertices[:1]
-                    else:
-                        polygon.vertices = polygon.vertices[-1:] + polygon.vertices[:-1]
+                if polygon.select and polygon.index != polygonactive:
+                    edges = [(polygon.vertices[i:]+polygon.vertices[:i])[:-2] for i in range(polygon.loop_total)]
+                    minangle = [0, 0]
+                    for i, edge in enumerate(edges):
+                        edgedirection = context.active_object.data.vertices[edge[1]].co - context.active_object.data.vertices[edge[0]].co
+                        edgedirection.normalize()
+                        angle = edgedirection.dot(polygonactivedirection) + 1   # cos() - from 0 to 2
+                        if minangle[1] < angle:
+                            minangle = [i, angle]
+                    polygon.vertices = polygon.vertices[minangle[0]:] + polygon.vertices[:minangle[0]]
             bpy.ops.object.mode_set(mode=activeobjectmode)
         return {'FINISHED'}
 
@@ -64,6 +64,7 @@ class MeshIntVars(bpy.types.PropertyGroup):
         ],
         default='cw'
     )
+
 
 def register():
     bpy.utils.register_class(MeshIntPolygonRotate)
