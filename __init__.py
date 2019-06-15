579e7debd94d7a1dc4094899c1bacb1ff0563f11
Nikita Akimov <korchiy@yandex.ru>
2019-06-15 08:52:12 +0300

    update

--- __init__.py
+++ __init__.py
@@ -4,6 +4,12 @@
 # GitHub
 # https://github.com/Korchy/blender-mesh-int
 
+from . import polygon_int_ops
+from . import polygon_int_panel
+from . import edges_int_ops
+from . import edges_int_panel
+from . import selection_int_ops
+
 bl_info = {
     'name': 'Mesh-Int',
     'category': 'Mesh',
@@ -16,27 +22,25 @@ bl_info = {
     'description': 'Mesh-Int - some additional tools for working with meshes'
 }
 
-from . import polygon_int_ops
-from . import polygon_int_panel
-from . import edges_int_ops
-from . import edges_int_panel
-from . import selection_int_ops
-
 
 def register():
-    selection_int_ops.register()
-    polygon_int_ops.register()
-    polygon_int_panel.register()
-    edges_int_ops.register()
-    edges_int_panel.register()
+    if not Addon.dev_mode():
+        selection_int_ops.register()
+        polygon_int_ops.register()
+        polygon_int_panel.register()
+        edges_int_ops.register()
+        edges_int_panel.register()
+    else:
+        print('It seems you are trying to use the dev version of the ' + bl_info['name'] + ' add-on. It may work not properly. Please download and use the release version!')
 
 
 def unregister():
-    polygon_int_ops.unregister()
-    polygon_int_panel.unregister()
-    edges_int_ops.unregister()
-    edges_int_panel.unregister()
-    selection_int_ops.unregister()
+    if not Addon.dev_mode():
+        polygon_int_ops.unregister()
+        polygon_int_panel.unregister()
+        edges_int_ops.unregister()
+        edges_int_panel.unregister()
+        selection_int_ops.unregister()
 
 
 if __name__ == '__main__':
